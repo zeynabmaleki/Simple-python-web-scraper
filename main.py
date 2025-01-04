ee9ed7ff8beb90592d06b128b62f50fc2e4e3733
Zaynab Maleki <zizimrad@gmail.com>
2025-01-04 18:34:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('index.html', 'r') as f:
-    doc = BeautifulSoup(f, 'html.parser')
+#with open('index.html', 'r') as f:
+#    doc = BeautifulSoup(f, 'html.parser')
+#tags = doc.find_all("p")
+#print(tags.string)
 
-print(doc.prettify())
+url = "https://www.scrapethissite.com/pages/"
+
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text, 'html.parser')
+
+header = doc.find_all ("h1")
+Parent = header[0].parent
+print(Parent)
+
